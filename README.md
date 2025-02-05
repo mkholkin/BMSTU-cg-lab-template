94fe8da26e63e2c1908fd71611b3d51aab4b99e4
mkholkin <mkholkin@MacBook-Pro-mkholkin.local>
2025-02-05 21:52:28 +0300

    Fix:
    - Added some to README.txt
    - Moved import of app lower in "run.py" so packages could install before import

---
+++ README.md
@@ -0,0 +1,9 @@
+Запусть приложение
+```shell
+python run.py
+```
+
+Запустить юнит тесты
+```shell
+python run_tests.py
+```
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import sys
 import subprocess
 
-from src.main import main as app
 
 if __name__ == "__main__":
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    from src.main import main as app
     app()
