cbcb8c62b025cd301a936b496cb017fe853f3075
mkholkin <mkholkin@MacBook-Pro-mkholkin.local>
2025-02-10 21:25:27 +0300

    Upd:
    - Mypy exclude works now
    - Testig report now contains coverage

--- src/view/ui/ui_main_window.py
+++ src/view/ui/ui_main_window.py
@@ -11,13 +11,8 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGraphicsView, QHBoxLayout, QMainWindow,
-    QMenuBar, QPushButton, QSizePolicy, QStatusBar,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QHBoxLayout, QMenuBar, QPushButton, QSizePolicy, QStatusBar,
+                               QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,19 @@ import datetime
 import json
 import os
 
+import coverage
+
 REPORT_FILE = os.path.join(os.path.dirname(__file__), "stud-unit-test-report.json")
 
 report_data = {
     "timestamp": datetime.datetime.now().astimezone().strftime("%Y-%m-%dT%H:%M:%S%:z"),
-    "coverage": 0,  # todo: implement coverage
+    "coverage": 0,
     "passed": 0,
     "failed": 0,
 }
+
+cov = coverage.Coverage(source=['src'])
+cov.start()
 
 
 def pytest_runtest_logreport(report):
@@ -21,5 +26,19 @@ def pytest_runtest_logreport(report):
 
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    cov.stop()
+    cov.save()
+
+    coverage_data = cov.get_data()
+    total_lines = 0
+    covered_lines = 0
+
+    for file in coverage_data.measured_files():
+        analysis = cov.analysis(file)
+        total_lines += len(analysis[1])
+        covered_lines += len(analysis[2])
+
+    report_data["coverage"] = (covered_lines / total_lines) if total_lines > 0 else 0
+
     with open(REPORT_FILE, "w") as f:
         json.dump(report_data, f, indent=4)
